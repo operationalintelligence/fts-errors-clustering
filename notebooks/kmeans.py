9c8a12df80122c7937abe4a4d2d9cad051754e97
Luca Clissa <luca.clissa2@unibo.it>
2020-03-11 13:11:52 +0100

    Add docstrings to kmeans module.

--- notebooks/kmeans.py
+++ notebooks/kmeans.py
@@ -1,5 +1,6 @@
 
 def kmeans_preproc(dataset, tks_vec):
+    """Take input dataset with Word2Vec representation in tks_vec column and properly format to feed into pyspark.ml.KMeans."""
     from pyspark.ml.feature import VectorAssembler
     vec_assembler = VectorAssembler(inputCols = [tks_vec], outputCol='features')
     dataset = vec_assembler.transform(dataset)
@@ -8,6 +9,21 @@ def kmeans_preproc(dataset, tks_vec):
 
 def train_kmeans(dataset, k, ft_col='features', distance="cosine", initSteps=10,
                  tol=0.0001, maxIter=30, save_path=None, mode="new"):
+    """Train K-Means model.
+    
+    -- params:
+    dataset (pyspark.sql.dataframe.DataFrame): data frame with a vector column with features for the kmeans algorithm
+    k (int): number of clusters
+    ft_col (string): name of the features column
+    distance ("euclidean" or "cosine"): distance measure for the kmeans algorithm
+    tol (int): tolerance for kmeans algorithm convergence
+    maxIter (int): maximum number of iterations for the kmeans algorithm
+    save_path (string): where to save trained kmeans model
+    mode ("new" or "overwrite"): whether to save new file or overwrite pre-existing one.
+
+    Returns:
+    model_fit (pyspark.ml.clustering.KMeansModel): trained K-Means model
+    """
     from pyspark.ml.clustering import KMeans
     
     model = KMeans(featuresCol=ft_col,k=k, initMode='k-means||',
@@ -23,12 +39,28 @@ def train_kmeans(dataset, k, ft_col='features', distance="cosine", initSteps=10,
     return(model_fit)
 
 def load_kmeans(save_path):
+    """Load K-Means model from save_path."""
     from pyspark.ml.clustering import KMeansModel
     model = KMeansModel.load(save_path)
     return(model)
     
 def K_optim(k_list, dataset, tks_vec="message_vector", ft_col="features", distance="cosine", 
             initSteps=10, tol=0.0001, maxIter=30):
+    """Train K-Means model for different K values.
+    
+    -- params:
+    k_list (list): grid of K values to try
+    dataset (pyspark.sql.dataframe.DataFrame): data frame with a vector column with features for the kmeans algorithm
+    tks_vec (string): name of the word2vec representations column
+    ft_col (string): name of the features column
+    distance ("euclidean" or "cosine"): distance measure for the kmeans algorithm
+    initStep (int): number of different random intializations for the kmeans algorithm
+    tol (int): tolerance for kmeans algorithm convergence
+    maxIter (int): maximum number of iterations for the kmeans algorithm
+
+    Returns:
+    res (dict): dictionary with grid of trained models and evaluation metrics. Keys:{"model", "wsse", "silhouette"}
+    """
     import time
     import datetime
     from pyspark.ml.evaluation import ClusteringEvaluator
@@ -62,10 +94,11 @@ def K_optim(k_list, dataset, tks_vec="message_vector", ft_col="features", distan
 
         print("\nTime elapsed: {} minutes and {} seconds.".format(int((time.time() - start_time)/60), int((time.time() - start_time)%60)))
         print('--'*30)
-        
-    return({"model": clustering_model, "wsse": wsse, "silhouette": silhouette})
+    res = {"model": clustering_model, "wsse": wsse, "silhouette": silhouette}
+    return(res)
 
 def plot_metrics(results):
+    """Plot the trends of evaluation metrics from the output of K_optim."""
     import numpy as np
     from matplotlib import pyplot as plt
 
@@ -92,6 +125,7 @@ def plot_metrics(results):
     return(None)
 
 def get_k_best(results, metric="silhouette"):
+    """Return the best K value according to the specified metric."""
     import numpy as np
     from matplotlib import pyplot as plt
 
@@ -108,6 +142,19 @@ def get_k_best(results, metric="silhouette"):
     
 def merge_predictions(original, predictions, orig_id="msg_id", pred_id="msg_id",
                       out_col_list=["tokens_cleaned", "abstract_tokens", "features", "prediction"]):
+    """Merge custering output with original dataset (from hdfs) 
+                    containing additional information (e.g. src/dst sites, timestamp, ...).
+    
+    -- params:
+    original (pyspark.sql.dataframe.DataFrame): data frame with dta from hdfs
+    predictions (pyspark.sql.dataframe.DataFrame): data frame with clustering results
+    orig_id (string): name of the message id column on hdfs data frame
+    perd_id (string): name of the message id column on prediction data frame
+    out_col_list (list[string]): list of names of the columns of the prediction data frame to append in the output
+
+    Returns:
+    merge_df (pyspark.sql.dataframe.DataFrame): merged data frame
+    """
     import numpy as np
     import pyspark.sql.functions as F
     
@@ -126,6 +173,20 @@ def merge_predictions(original, predictions, orig_id="msg_id", pred_id="msg_id",
 
 
 def kmeans_predict(dataset, model, pred_mode="static", new_cluster_thresh=None, update_model_path=None):
+    """Predict cluster for new observations.
+    
+    -- params:
+    dataset (pyspark.sql.dataframe.DataFrame): data frame with a vector column with features for the kmeans algorithm
+    model (pyspark.ml.clustering.KMeansModel): re-trained kmeans model
+    pred_mode ("static" or "update"): prediction mode: "static" does not allow for creating new clusters
+    distance ("euclidean" or "cosine"): distance measure for the kmeans algorithm
+    new_cluster_thresh (float): distance threshold: if closest centroid is more distant than new_cluster_thresh 
+                                then a new cluster is created for the new observation
+    update_model_path (string): where to save update kmeans model
+    
+    Returns:
+    pred (pyspark.sql.dataframe.DataFrame): the input data frame with an extra "prediction" column
+    """
     if pred_mode not in ["static", "update"]:
         print("""WARNING: invalid param \"pred_mode\". Specify either \"static\" to train load a pre-trained model 
               or \"update\" to train it online.""")
@@ -139,13 +200,13 @@ def kmeans_predict(dataset, model, pred_mode="static", new_cluster_thresh=None, 
         # initialize new clusters when closest centroid distance is greater than new_cluster_thresh
         # update centroids and points in each cluster
         # save updated model
-        update_model_path = "temp_ciccio" # temporary to avoid accidental overwriting
+        update_model_path = "temp_filename" # temporary to avoid accidental overwriting
         model.write().overwrite().save(update_model_path)
         pred = None
     return(pred)
     
 def kmeans_inference(original_data, msg_col, id_col, w2v_model_path, tks_vec, ft_col, kmeans_mode, kmeans_model_path,
-                     pred_mode="static", new_cluster_thresh=None, #update_model_path=None, #kmeans_predit
+                     pred_mode="static", new_cluster_thresh=None, #update_model_path=None,
                      distance="cosine", opt_initSteps=10, opt_tol=0.0001, opt_maxIter=30, #K_optim
                      tr_initSteps=200, tr_tol=0.000001, tr_maxIter=100, #train_kmeans
                     ):
@@ -154,7 +215,31 @@ def kmeans_inference(original_data, msg_col, id_col, w2v_model_path, tks_vec, ft
     import time
     import datetime
     from pyspark.ml.evaluation import ClusteringEvaluator
-
+    """Perform inference on new error messages (Note: only K-Means can be re-trained/updated).
+    
+    -- params:
+    original_data (pyspark.sql.dataframe.DataFrame): data frame with at least error string and id columns
+    msg_col (string): name of the error string column
+    id_col (string): name of the message id column
+    model_path (string): path where to load pre-trained word2vec model
+    tks_vec (string): name of the word2vec representations column
+    ft_col (string): name of the features column
+    kmeans_mode ("load" or "train"): kmeans mode: "load" uses pre-trained model, while "train" performs online training
+    kmeans_model_path (string): path to pre-trained model (Specify None for re-training)
+    pred_mode ("static" or "update"): prediction mode: "static" does not allow for creating new clusters
+    new_cluster_thresh (float): distance threshold: if closest centroid is more distant than new_cluster_thresh 
+                                then a new cluster is created for the new observation
+    distance ("euclidean" or "cosine"): distance measure for the kmeans algorithm
+    opt_initStep (int): number of different random intializations for the kmeans algorithm in the optimization phase
+    opt_tol (int): tolerance for kmeans algorithm convergence in the optimization phase
+    opt_maxIter (int): maximum number of iterations for the kmeans algorithm in the optimization phase
+    tr_initStep (int): number of different random intializations for the kmeans algorithm in the training phase
+    tr_tol (int): tolerance for kmeans algorithm convergence in the training phase
+    tr_maxIter (int): maximum number of iterations for the kmeans algorithm in the training phase
+    
+    Returns:
+    original_data (pyspark.sql.dataframe.DataFrame): the input data frame with an extra "prediction" column
+    """
     if kmeans_mode not in ["load", "train"]:
         print("""WARNING: invalid param \"kmeans_mode\". Specify either \"load\" to train load a pre-trained model 
               or \"train\" to train it online.""")
